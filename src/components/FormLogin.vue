96d36869e3bd39763f57b9370d21a0ce3d29c9e3
David Celis <davidcelis682@gmail.com>
2022-10-27 09:29:46 -0500

    Update

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,82 +1,123 @@
 <template>
-        <div class="login">
-            <div class="formulario">
-                <div class="titleLogin">
-                    <h1>INICIO DE SESIÓN</h1>
-                </div>
-                <div class="fields">
-                    <h3>USUARIO:</h3>
-                    <input type="text">
-                    <h3>CONTRASEÑA:</h3>
-                    <input type="password">
-                    <div class="btn">
-                        <button class="boton">INICIAR SESIÓN</button>
-                    </div>
+    <div class="login">
+        <form class="formulario" v-on:submit.prevent="login">
+            <div class="titleLogin">
+                <h1>INICIO DE SESIÓN</h1>
+            </div>
+            <div class="fields">
+                <h3>USUARIO:</h3>
+                <input type="text" v-model="user.Nick_usu">
+                <h3>CONTRASEÑA:</h3>
+                <input type="password" v-model="user.Contra_usu">
+                <div class="btn">
+                    <button class="boton" type="submit">INICIAR SESIÓN</button>
                 </div>
             </div>
-        </div>    
-    
+        </form>
+    </div>
+
 </template>
 
 <script>
-    export default {
-    name: 'FormLogin'
+import axios from "axios";
+import Swal from "sweetalert2";
+export default {
+    name: 'FormLogin',
+    data() {
+        return {
+            user: {
+                Nick_usu: "",
+                Contra_usu: ""
+            }
+        }
+    },
+    methods: {
+        login() {
+            axios.post("http://localhost:3000/user/auth/signin", this.user)
+                .then((result) => {
+                    if (result.data.success) {
+                        sessionStorage.setItem("jwt", result.data.body.token);
+                        Swal.fire({
+                            icon: "success",
+                            title: "Sesion iniciada",
+                            showConfirmButton: false,
+                            timer: 1000,
+                        });
+                        this.$router.push({ path: '/Menu' });
+                    } else {
+                        Swal.fire({
+                            icon: "error",
+                            title: "Usuario y/o contraseña incorrecta",
+                            showConfirmButton: false,
+                            timer: 1200,
+                        });
+                    }
+                })
+                .catch((error) => {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Usuario y/o contraseña incorrecta",
+                        showConfirmButton: false,
+                        timer: 1200,
+                    });
+                });
+        }
     }
+}
 </script>
 
 <style>
+.login {
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(#0C853B, #24A321);
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 
-    .login{
-        height:100%;
-        width: 100%;
-        background: linear-gradient(#0C853B, #24A321);
-        color: white;
-        display: flex;
-        align-items: center;
-        justify-content: center;   
-    }
+.titleLogin {
+    text-align: center;
+    font-size: 25px;
+    padding-bottom: 90px;
+}
 
-    .titleLogin{
-        text-align: center;
-        font-size: 25px;
-        padding-bottom: 90px;
-    }
+.fields {
+    font-size: 30px;
 
-    .fields{
-        font-size: 30px;
+}
 
-    } 
-    
-   .formulario input{
-        width: 430px;
-        height: 60px;
-        border-radius: 25px;
-        border: none;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        margin-top: 20px;
-        margin-bottom: 20px;
-        font-size: 30px;
-        padding-left: 10px;
-    }
+.formulario input {
+    width: 430px;
+    height: 60px;
+    border-radius: 25px;
+    border: none;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-size: 30px;
+    padding-left: 10px;
+}
 
-    .btn{
-        align-content: center;
-        justify-content: center;
-        text-align: center;
-      
-    }
-    .boton{
-        height: 55px;
-        width: 250px;
-        border-radius: 25px;
-        background-color: #EFBF25;
-        border: none;
-        font-size: 25px;
-        color: white;
-        font-weight: bold;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        margin-top: 20px;
-    }
+.btn {
+    align-content: center;
+    justify-content: center;
+    text-align: center;
 
+}
+
+.boton {
+    height: 55px;
+    width: 250px;
+    border-radius: 25px;
+    background-color: #EFBF25;
+    border: none;
+    font-size: 25px;
+    color: white;
+    font-weight: bold;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    margin-top: 20px;
+}
 </style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Menu from '../views/Menu.vue'
-
+import axios from "axios";
 const routes = [
   {
     path: '/',
@@ -14,12 +14,35 @@ const routes = [
     name: 'Menu',
     component: Menu
   },
-  
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach(async (to, from) => {
+  if (
+    !sessionStorage.getItem('jwt') &&
+    to.name !== 'Login'
+  ) {
+    return { name: 'Login' }
+  }else{
+    let token = sessionStorage.getItem('jwt');
+    let res = await axios.post("http://localhost:3000/user/auth/verifyToken", {token})
+    .then((result)=>{
+      sessionStorage.setItem("User", result.data.body.decoded.user.Nombre_usu);
+      sessionStorage.setItem("Codigo_usu", result.data.body.decoded.user.Codigo_usu);
+      sessionStorage.setItem("Tipo", result.data.body.decoded.user.Tipo_usu);
+      return result.data.success
+    }).catch((err)=>{
+      return false;
+    });
+    if(!res && to.name !== 'Login'){
+      return { name: 'Login' }
+    }
+  }
+
 })
 
 export default router
