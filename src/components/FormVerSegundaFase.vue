19f635b0836677ecf80a38340cdefb24c29277dd
David Celis <davidcelis682@gmail.com>
2022-11-05 10:15:15 -0500

    Update

--- src/components/FormVerSegundaFase.vue
+++ src/components/FormVerSegundaFase.vue
@@ -85,7 +85,7 @@
                         <td>{{ firma.Nombre_usu }}</td>
                         <td>{{ firma.Tipo_usu }}</td>
                         <td>
-                            <img src="../assets/icon_user.png" alt="" v-show="firma.Firma_desarrollo == 1">
+                            <img src="../assets/iconDone.png" alt="" v-show="firma.Firma_desarrollo == 1">
                             <img src="../assets/iconX.png" alt="" v-show="firma.Firma_desarrollo == 0">
                         </td>
 
